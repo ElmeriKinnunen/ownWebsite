6c5c2b684bba0c536a5eb009f95295aee632e8c4
Elmeri Kinnunen <elmeri.kin@gmail.com>
2020-12-15 23:55:54 +0200

    info component updated

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,20 +4,23 @@ import "./components.css"
 const Info=()=> {
     return (
       <div className='component info'>
-        
-        <div className='textBox' id='leftBox'>
-            <h2>Ohjelmistokehitys</h2>
-            <p>On todistettu, että lukijaa häiritsee sivun ulkoasu lukiessaan sivua. Lorem Ipsumin käytön tarkoitus on, että se omaa enemmän-tai-vähemmän normaalimpaa sanojen sijoitusta kuin 'Sisältöä tähän, sisältöä tähän'.</p>
+        <div className='infoHeader'>
+                <h1>Elmeri <br/> Kinnunen</h1>
         </div>
-        <div className='textBox' id='centerBox'>
-            <h2>Design</h2>
-            <p>Vastoin yleistä uskomusta, Lorem Ipsum ei ole vain sattumanvarainen teksti. Sillä on pitkät juuret klassisesta latinalaisesta kirjallisuudesta vuonna 45 eKr alkaen, tehden siitä yli 2000 vuotta vanhan.</p>
+        <div className='infoRow'>
+          <div className='textBox' id='leftBox'>
+              <h2>Ohjelmistokehitys</h2>
+              <p>On todistettu, että lukijaa häiritsee sivun ulkoasu lukiessaan sivua. Lorem Ipsumin käytön tarkoitus on, että se omaa enemmän-tai-vähemmän normaalimpaa sanojen sijoitusta kuin 'Sisältöä tähän, sisältöä tähän'.</p>
+          </div>
+          <div className='textBox' id='centerBox'>
+              <h2>Design</h2>
+              <p>Vastoin yleistä uskomusta, Lorem Ipsum ei ole vain sattumanvarainen teksti. Sillä on pitkät juuret klassisesta latinalaisesta kirjallisuudesta vuonna 45 eKr alkaen, tehden siitä yli 2000 vuotta vanhan.</p>
+          </div>
+          <div className='textBox' id='rightBox'>
+              <h2>Robotiikka</h2>
+              <p>On olemassa monta eri versiota Lorem Ipsumin kappaleista, mutta suurin osa on kärsinyt muunnoksista joissain muodoissa, kuten huumorin tai sattumanvaraisesti asetetuin sanoin jotka eivät näytä edes vähän uskottavalta. </p>
+          </div>
         </div>
-        <div className='textBox' id='rightBox'>
-            <h2>Robotiikka</h2>
-            <p>On olemassa monta eri versiota Lorem Ipsumin kappaleista, mutta suurin osa on kärsinyt muunnoksista joissain muodoissa, kuten huumorin tai sattumanvaraisesti asetetuin sanoin jotka eivät näytä edes vähän uskottavalta. Jos sinä aiot käyttää kappaletta Lorem Ipsumista, sinun pitää tarkistaa, ettei tekstin seassa ole mitään nolostuttavaa.</p>
-        </div>
-      
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,8 +5,10 @@ import logo from "./../images/logo.png"
 const Landing=()=> {
     return (
     <div className='component landing'>
-        <img src={logo} alt="logo" />
-        <h1>Elmeri <br/> Kinnunen</h1>
+        <div className='leftBackround'>
+            <img className='logo' src={logo} alt="logo" />
+            
+        </div>
     </div>
 
     );
